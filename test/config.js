f3f8336ed1f0600c947edad6bd02b619cb49849a
Noah Grant <foolproofnoah@gmail.com>
2019-05-10 18:54:53 -0700

    add config test suite

---
+++ test/config.js
@@ -0,0 +1,104 @@
+import * as Config from '../lib/config';
+import Backbone from 'backbone';
+import {noOp} from '../lib/utils';
+
+const TestModel = Backbone.Model.extend();
+const TestModel2 = Backbone.Model.extend();
+
+/* eslint-disable max-nested-callbacks */
+describe('Config', () => {
+  afterEach(() => {
+    delete Config.ResourceKeys.TEST_MODEL;
+    delete Config.ResourceKeys.TEST_MODEL2;
+  });
+
+  describe('#addResourceKeys', () => {
+    it('adds keys to the ResourceKeys object', () => {
+      expect(Config.ResourceKeys.TEST_MODEL).not.toBeDefined();
+      expect(Config.ResourceKeys.TEST_MODEL2).not.toBeDefined();
+
+      Config.addResourceKeys({TEST_MODEL: 'testModel', TEST_MODEL2: 'testModel2'});
+
+      expect(Config.ResourceKeys.TEST_MODEL).toEqual('testModel');
+      expect(Config.ResourceKeys.TEST_MODEL2).toEqual('testModel2');
+    });
+  });
+
+  describe('#addModels', () => {
+    beforeEach(() => {
+      expect(Config.ModelMap.testModel).not.toBeDefined();
+      expect(Config.ModelMap.testModel2).not.toBeDefined();
+    });
+
+    afterEach(() => {
+      delete Config.ModelMap.testModel;
+      delete Config.ModelMap.testModel2;
+    });
+
+    it('adds models passed as an object to the model map', () => {
+      Config.addModels({testModel: TestModel, testModel2: TestModel2});
+
+      expect(Config.ModelMap.testModel).toEqual(TestModel);
+      expect(Config.ModelMap.testModel2).toEqual(TestModel2);
+    });
+
+    it('adds models returned from a function to the model map', () => {
+      Config.addResourceKeys({TEST_MODEL: 'testModel', TEST_MODEL2: 'testModel2'});
+
+      Config.addModels((ResourceKeys) => ({
+        [ResourceKeys.TEST_MODEL]: TestModel,
+        [ResourceKeys.TEST_MODEL2]: TestModel2
+      }));
+
+      expect(Config.ModelMap.testModel).toEqual(TestModel);
+      expect(Config.ModelMap.testModel2).toEqual(TestModel2);
+    });
+  });
+
+  describe('#addUnfetchedResources', () => {
+    it('adds a resource to the unfetched list', () => {
+      Config.addResourceKeys({TEST_MODEL: 'testModel', TEST_MODEL2: 'testModel2'});
+      expect(Config.UnfetchedResources.has(Config.ResourceKeys.TEST_MODEL)).toBe(false);
+      expect(Config.UnfetchedResources.has(Config.ResourceKeys.TEST_MODEL2)).toBe(false);
+
+      Config.addUnfetchedResources(({TEST_MODEL, TEST_MODEL2}) => [TEST_MODEL, TEST_MODEL2]);
+
+      expect(Config.UnfetchedResources.has(Config.ResourceKeys.TEST_MODEL)).toBe(true);
+      expect(Config.UnfetchedResources.has(Config.ResourceKeys.TEST_MODEL2)).toBe(true);
+
+      Config.UnfetchedResources.delete(Config.ResourceKeys.TEST_MODEL);
+      Config.UnfetchedResources.delete(Config.ResourceKeys.TEST_MODEL2);
+    });
+  });
+
+  describe('#setConfig', () => {
+    it('adds a setting to the configs, overriding defaults', () => {
+      var logSpy = jasmine.createSpy('log'),
+          trackSpy = jasmine.createSpy('track');
+
+      expect(Config.ResourcesConfig.cacheGracePeriod).toEqual(120000);
+      expect(Config.ResourcesConfig.log).toEqual(noOp);
+      expect(Config.ResourcesConfig.track).toEqual(noOp);
+      expect(Config.ResourcesConfig.queryParamsPropName).toEqual('urlParams');
+
+      Config.setConfig({
+        cacheGracePeriod: 300000,
+        log: logSpy,
+        track: trackSpy,
+        queryParamsPropName: 'queryPs'
+      });
+
+      expect(Config.ResourcesConfig.cacheGracePeriod).toEqual(300000);
+      expect(Config.ResourcesConfig.log).toEqual(logSpy);
+      expect(Config.ResourcesConfig.track).toEqual(trackSpy);
+      expect(Config.ResourcesConfig.queryParamsPropName).toEqual('queryPs');
+
+      Config.setConfig({
+        cacheGracePeriod: 120000,
+        log: noOp,
+        track: noOp,
+        queryParamsPropName: 'queryParamsPropName'
+      });
+    });
+  });
+});
