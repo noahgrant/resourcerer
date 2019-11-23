77fe286c6e5ab5a6fa28a4059a68db78fb2a1739
Noah Grant <foolproofnoah@gmail.com>
2019-11-22 19:56:54 -0800

    add "direct" option to withLoadingOverlay

--- test/with-loading-overlay.jsx
+++ test/with-loading-overlay.jsx
@@ -1,21 +1,22 @@
+import withLoadingOverlay, {NoUpdateIfLoading} from '../lib/with-loading-overlay.js';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {ResourcesConfig} from '../lib/config';
 import {scryRenderedComponentsWithType} from 'react-dom/test-utils';
 import {waitsFor} from './test-utils';
-import withLoadingOverlay from '../lib/with-loading-overlay.js';
 
-const TestClass = class TestComponent extends React.Component {
+class TestComponent extends React.Component {
   render() {
     return <div />;
   }
-};
+}
 
 const {Loader} = ResourcesConfig;
 const jasmineNode = document.createElement('div');
 
 describe('WithLoadingOverlay', () => {
-  var Loady = withLoadingOverlay()(TestClass),
+  var Loady = withLoadingOverlay()(TestComponent),
       loady,
       loaders,
 
@@ -41,21 +42,34 @@ describe('WithLoadingOverlay', () => {
     expect(loaders[0].props.overlay).toBe(true);
   });
 
-  it('renders the wrapped component only after \'hasLoaded\' has been true once', (done) => {
+  it('renders the wrapped component only after \'hasLoaded\' has been true once', async(done) => {
     loady = renderLoady();
-    expect(scryRenderedComponentsWithType(loady, TestClass).length).toEqual(0);
+    expect(scryRenderedComponentsWithType(loady, TestComponent).length).toEqual(0);
     loady = renderLoady({hasLoaded: true});
 
-    return waitsFor(() => loady.state.hasInitiallyLoaded).then(() => {
-      expect(scryRenderedComponentsWithType(loady, TestClass).length).toEqual(1);
-      done();
-    });
+    await waitsFor(() => loady.state.hasInitiallyLoaded);
+
+    expect(scryRenderedComponentsWithType(loady, TestComponent).length).toEqual(1);
+    done();
   });
 
   it('does not show the spinner if passed a \'noLoader\' option', () => {
-    loady = renderLoady({isLoading: true}, withLoadingOverlay({noLoader: true})(TestClass));
+    loady = renderLoady({isLoading: true}, withLoadingOverlay({noLoader: true})(TestComponent));
     loaders = scryRenderedComponentsWithType(loady, Loader);
 
     expect(loaders.length).toEqual(0);
+  });
+
+  it('does not include its own intermediary component if passed a \'direct\' option', () => {
+    // pass hasErrored just as an easy way to render its children
+    loady = renderLoady({hasErrored: true});
+    expect(scryRenderedComponentsWithType(loady, NoUpdateIfLoading).length).toEqual(1);
+    expect(scryRenderedComponentsWithType(loady, NoUpdateIfLoading)[0].props.Component)
+        .toEqual(TestComponent);
+    ReactDOM.unmountComponentAtNode(jasmineNode);
+
+    loady = renderLoady({hasErrored: true}, withLoadingOverlay({direct: true})(TestComponent));
+    expect(scryRenderedComponentsWithType(loady, NoUpdateIfLoading).length).toEqual(0);
+    expect(loady.child instanceof TestComponent).toBe(true);
   });
 });
