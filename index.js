5080348973a6989846a4da7c315603cf9e9c3e53
Noah Grant <foolproofnoah@gmail.com>
2021-10-18 15:53:38 -0700

    response semantics

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export {default as Collection} from './lib/collection';
 export {default as Model} from './lib/model';
-export {default as sync, ajax} from './lib/model';
+export {default as sync, ajax} from './lib/sync';
 export {default as prefetch} from './lib/prefetch.js';
 export {default as request} from './lib/request.js';
 export {default as ModelCache} from './lib/model-cache.js';
