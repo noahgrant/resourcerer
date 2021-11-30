2ff8665bf87d893932fd3b7f2b437cf656a22642
Noah Grant <1355779+noahgrant@users.noreply.github.com>
2021-11-29 20:08:17 -0800

    remove legacy measure code (#106)

--- test/use-resources.test.jsx
+++ test/use-resources.test.jsx
@@ -786,35 +786,6 @@ describe('useResources', () => {
       expect(ResourcesConfig.track).not.toHaveBeenCalled();
     });
 
-    describe('that when set to true', () => {
-      beforeEach(async() => {
-        measure = true;
-        jest.spyOn(ModelCache, 'get').mockReturnValue();
-        dataChild = findDataChild(renderUseResources());
-        await waitsFor(() => dataChild.props.hasLoaded);
-      });
-
-      afterEach(() => {
-        ModelCache.get.mockRestore();
-        measure = false;
-      });
-
-      it('measures the request time', () => {
-        expect(markName).toEqual(ResourceKeys.DECISIONS);
-        expect(measureCount).toEqual(1);
-        expect(measureName).toEqual(ResourceKeys.DECISIONS);
-      });
-
-      it('tracks the request', () => {
-        expect(ResourcesConfig.track).toHaveBeenCalledWith('API Fetch', {
-          Resource: ResourceKeys.DECISIONS,
-          data: undefined,
-          options: undefined,
-          duration: 5
-        });
-      });
-    });
-
     describe('as a static property', () => {
       beforeEach(() => {
         jest.spyOn(ModelCache, 'get').mockReturnValue();
