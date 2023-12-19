ec7464e65994d12508baff2e5c2da5e75226b0c4
Noah Grant <foolproofnoah@gmail.com>
2023-12-18 22:20:22 -0800

    type: module and no jsx

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-export {default as Collection} from './lib/collection';
-export {default as Model} from './lib/model';
-export {default as sync, ajax} from './lib/sync';
+export {default as Collection} from './lib/collection.js';
+export {default as Model} from './lib/model.js';
+export {default as sync, ajax} from './lib/sync.js';
 export {default as prefetch} from './lib/prefetch.js';
 export {default as request} from './lib/request.js';
 export {default as ModelCache} from './lib/model-cache.js';
