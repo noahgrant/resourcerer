60c9f671b519f5e9926bda387a62e8ab535bb16f
Noah Grant <1355779+noahgrant@users.noreply.github.com>
2024-07-29 08:16:14 -0700

    Executor Fn second generic arg parameters (#171)

--- resourcerer.d.ts
+++ resourcerer.d.ts
@@ -1,19 +1,27 @@
+import Model from "./build/lib/model.js";
 import Collection from "./build/lib/collection.js";
 
 export * from "./build/index.js";
+
+type GetModelOptions<C> =
+  C extends Model<infer A, infer O> ? [A, O]
+  : C extends Collection<infer M, infer O> ? [M, O]
+  : never;
 
 declare module "resourcerer" {
   type WithModelSuffix<K extends ResourceKeys, C> =
     C extends Collection<any, any> ? `${K}Collection` : `${K}Model`;
   export type LoadingStates = "error" | "loading" | "loaded" | "pending";
   export type ResourceConfigObj<K extends ResourceKeys> = {
-    data?: { [key: string]: any };
+    data?: Partial<GetModelOptions<InstanceType<ModelMap[K]>>[0]>;
     dependsOn?: boolean;
     force?: boolean;
     lazy?: boolean;
     noncritical?: boolean;
     options?: { [key: string]: any };
-    path?: { [key: string]: any };
+    // ts is not happy with this but this will make our typings easier
+    // @ts-ignore
+    path?: Parameters<InstanceType<ModelMap[K]>["url"]>[0];
     params?: { [key: string]: any };
     prefetches?: { [key: string]: any }[];
     provides?: (
@@ -25,7 +33,7 @@ declare module "resourcerer" {
   export interface ModelMap {}
   export type ResourceKeys = Extract<keyof ModelMap, string>;
 
-  export type ExecutorFunction<T extends ResourceKeys, O = any> = (props: O) => {
+  export type ExecutorFunction<T extends ResourceKeys, O = Record<string, never>> = (props: O) => {
     [Key in T]?: ResourceConfigObj<Key>;
   };
 
